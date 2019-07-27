7a1e3fcbdc2fb0bd55cf7fd6c5c665bad48bb60a
Tao <ctaohe@gmail.com>
2019-07-27 15:31:27 +0800

    init file

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const db = mongoose.connection
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.post('/', (req, res) => {
+  res.render('result')
+})
+
+app.listen(port, () => {
+  console.log(`App is running on ${port}`)
+})
