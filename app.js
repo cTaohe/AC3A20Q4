3050607a9587adc9a751b441319f3eaecd645adf
Tao <ctaohe@gmail.com>
2019-07-27 21:55:55 +0800

    Add random shotr url

--- app.js
+++ app.js
@@ -3,19 +3,68 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
 const mongoose = require('mongoose')
+const password = require('./password')
+const Url = require('./models/url')
 
 const db = mongoose.connection
+mongoose.connect('mongodb://localhost/url', { useNewUrlParser: true })
+db.on('error', () => {
+  console.log('mongoose error')
+})
+db.once('open', () => {
+  console.log('mongoose connected')
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(flash())
 
 app.get('/', (req, res) => {
   res.render('index')
 })
-app.post('/', (req, res) => {
-  res.render('result')
+
+app.post('/url', async (req, res) => {
+
+  const { url } = req.body
+  let errors = []
+  // 產生新 url
+  const shortnewUrl = await password.shortUrl()
+
+  // 檢查原始網址是否存在
+  Url.findOne({ url: url }).then(urls => {
+    // 產生5碼亂碼
+    if (urls) {
+      errors.push({ message: `原來網址${urls.url} 已有產生的短網址http://localhost/${urls.shorten}` })
+    }
+
+    if (errors.length > 0) {
+      res.render('index', { errors })
+    } else {
+      const newUrl = new Url({
+        url: req.body.url,
+        shorten: shortnewUrl
+      })
+      // 儲存到資料庫
+      newUrl.save(error => {
+        if (error) return console.error(error)
+        return res.render('result')
+      })
+    }
+  }).catch((err) => { console.log(err) })
+})
+
+app.get('/:shorten', async (req, res) => {
+  const shurl = req.params.shorten
+  const orgrl = await Url.findOne({shorten: shurl}).exec()
+
+  if (orgrl) {
+    let urlOrg = orgrl.url
+    res.redirect(urlOrg)
+  }
 })
 
 app.listen(port, () => {
---
+++ password.js
@@ -0,0 +1,17 @@
+const Url = require('./models/url')
+
+module.exports = {
+  shortUrl : async () => {
+    let randomText = ''
+    do {
+      const stringPossible = 'abcdfrghijklmnopqrlstuvxyzABCDEFGHIJKLMNOPQRLSTUVXYZ0123456789'
+      for (let i = 0; i < 5; i++) {
+        randomText += stringPossible.charAt(Math.floor(Math.random() * stringPossible.length))
+      }
+      const newUrl = await Url.findOne({ shorten: randomText })
+      if (!newUrl) {
+        return randomText
+      }
+    } while (newUrl)
+  }
+}
